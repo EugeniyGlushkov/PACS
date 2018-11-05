d01b5596df756ff94a4193fb3b76f05314535736
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2018-11-05 22:28:40 +0300

    complete initDB, start model, add util

---
+++ src/main/java/ru/alvisid/pacs/util/ValidationUtil.java
@@ -0,0 +1,69 @@
+package ru.alvisid.pacs.util;
+
+import org.acs.util.exceptions.NotFoundException;
+import ru.alvisid.pacs.util.exceptions.NotFoundException;
+
+import java.util.Objects;
+
+/**
+ * This class consists exclusively of static methods wich helps
+ * with checking the passed values.
+ */
+public class ValidationUtil {
+    private ValidationUtil() {
+
+    }
+
+    /**
+     * Throws the NotFoundException if a found is false.
+     *
+     * @param found   shows availability the entity in the DB.
+     * @param message message for thrown NotFoundException.
+     * @throws NotFoundException the exception is thrown when entity is not found.
+     * @see ValidationUtil#checkNotFound(Object, String)
+     */
+    public static void checkNotFound(boolean found, String message) {
+        if (!found) {
+            throw new NotFoundException(message);
+        }
+    }
+
+    /**
+     * Returns a checked object, throws the NotFoundException if the checked object is null.
+     *
+     * @param object the checked object.
+     * @param message message for thrown NotFoundException..
+     * @return the checked object.
+     * @throws NotFoundException if the checked object is null.
+     * @see ValidationUtil#checkNotFound(boolean, String).
+     */
+    public static <T> T checkNotFound(T object, String message) {
+        checkNotFound(!Objects.isNull(object), message);
+        return object;
+    }
+
+    /**
+     * Throws the NotFoundException if a found is false.
+     *
+     * @param found shows availability the entity in the DB.
+     * @param id specific id of the entity.
+     * @throws NotFoundException the exception is thrown when entity is not found.
+     * @see ValidationUtil#checkNotFoundWithId(Object, int).
+     */
+    public static void checkNotFoundWithId(boolean found, int id) {
+        checkNotFound(found, "Not found entity with id=" + id);
+    }
+
+    /**
+     * Returns a checked object, throws the NotFoundException if the checked object is null.
+     *
+     * @param object the checked object.
+     * @param id specific id of the entity.
+     * @return the checked object.
+     * @throws NotFoundException if the checked object is null.
+     * @see ValidationUtil#checkNotFoundWithId(boolean, int).
+     */
+    public static <T> T checkNotFoundWithId(T object, int id) {
+        return checkNotFound(object, "Not found entity with id=" + id);
+    }
+}
---
+++ src/main/java/ru/alvisid/pacs/util/exceptions/NotFoundException.java
@@ -0,0 +1,18 @@
+package ru.alvisid.pacs.util.exceptions;
+
+/**
+ * The exception is thrown when entity is not found.
+ *
+ * @author Glushkov Evgeniy
+ * @version 1.0
+ */
+public class NotFoundException extends RuntimeException {
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     *
+     * @param message the detail message
+     */
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
