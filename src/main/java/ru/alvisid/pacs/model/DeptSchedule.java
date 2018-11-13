d1a74a519a29a58720b2ee6efd545520df12067a
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2018-11-13 10:41:03 +0300

    add annotations to the DeptSchedule class

--- src/main/java/ru/alvisid/pacs/model/DeptSchedule.java
+++ src/main/java/ru/alvisid/pacs/model/DeptSchedule.java
@@ -1,8 +1,24 @@
 package ru.alvisid.pacs.model;
 
+import org.hibernate.annotations.Check;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalTime;
 
+@Entity
+@Table(name = "dep_schedules", uniqueConstraints =
+@UniqueConstraint(columnNames = "dep_id", name = "depsched_unique_depid_idx"))
+@Check(constraints = "start_work < start_lunch\n" +
+        "AND start_lunch < end_lunch\n" +
+        "AND end_lunch < end_work")
 public class DeptSchedule extends AbstractSchedule{
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dep_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull
     private Department department;
 
     public Department getDepartment() {
