5467438e07037e4e15b39663c412dabaee289d6f
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2018-11-15 08:49:26 +0300

    Add EditType.

---
+++ src/main/java/ru/alvisid/pacs/model/EditType.java
@@ -0,0 +1,13 @@
+package ru.alvisid.pacs.model;
+
+/**
+ *Represent the types of edits which made by users.
+ *
+ * @author Glushkov Evgeniy
+ * @version 1.0
+ */
+public enum EditType {
+    CREATE,
+    UPDATE,
+    DELETE
+}
