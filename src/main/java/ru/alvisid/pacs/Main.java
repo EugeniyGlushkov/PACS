bb9d1062b7823125471991bbaa2b0e3687d47c4e
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2018-11-20 11:59:11 +0300

    config log.

--- src/main/java/ru/alvisid/pacs/Main.java
+++ src/main/java/ru/alvisid/pacs/Main.java
@@ -1,8 +1,13 @@
 package ru.alvisid.pacs;
 
 import org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class Main {
+    private static final Logger log = getLogger(Main.class);
     public static void main(String[] args) {
+        log.debug("In method Main");
     }
 }
