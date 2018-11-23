106a0f3b6dcdd3a0399ea45ad440bf31bc2738f2
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2018-11-23 16:52:50 +0300

    Delete session in the loader, new metod updateEnumValues

--- src/main/java/ru/alvisid/pacs/Main.java
+++ src/main/java/ru/alvisid/pacs/Main.java
@@ -1,22 +1,14 @@
 package ru.alvisid.pacs;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.alvisid.pacs.model.EditType;
 import ru.alvisid.pacs.model.WeekDay;
-import ru.alvisid.pacs.model.enumActivate.AbstractDictionary;
-import ru.alvisid.pacs.model.enumActivate.WeekDayDict;
 
-import javax.persistence.EntityManager;
-import javax.persistence.metamodel.EntityType;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.slf4j.LoggerFactory.getLogger;
-import static ru.alvisid.pacs.model.WeekDay.*;
 
 public class Main {
     private static final Logger log = getLogger(Main.class);
@@ -26,36 +18,14 @@ public class Main {
         ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-db.xml");
         System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
         Enum[] days = WeekDay.values();
-        System.out.println(days.length);
+        System.out.println(days.length + "\n");
         for (Enum e : days) {
             System.out.println(e == null ? 0 : e.ordinal() );
         }
         days = EditType.values();
-        System.out.println(days.length);
+        System.out.println(days.length + "\n");
         for (Enum e : days) {
             System.out.println(e == null ? 0 : e.ordinal() );
         }
-
-        /*EntityManager em = ((Cont)appCtx.getBean(Cont.class)).getEm();
-        System.out.println(em);
-        Set<EntityType<?>> entities = em.getMetamodel().getEntities();
-        List<?> classes = entities.stream()
-                .map(EntityType::getJavaType)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-        classes.forEach(System.out::println);
-        Session session = (Session)em.getDelegate();
-        System.out.println(session);
-        SessionFactory sf = session.getSessionFactory();
-        Session newSession = sf.openSession();
-        System.out.println(newSession);
-        System.out.println(MONDAY.ordinal());
-        String sql = "FROM " + WeekDayDict.class.getSimpleName();
-        System.out.println("sql = " + sql);
-
-        List<AbstractDictionary> weekDays = em.createQuery(sql).getResultList();
-        System.out.println(weekDays);
-
-        System.out.println(AbstractDictionary.class.isAssignableFrom(((Object)(new WeekDayDict())).getClass()));*/
     }
 }
--- src/main/java/ru/alvisid/pacs/repository/loader/EnumLoader.java
+++ src/main/java/ru/alvisid/pacs/repository/loader/EnumLoader.java
@@ -1,6 +1,5 @@
 package ru.alvisid.pacs.repository.loader;
 
-import org.hibernate.Session;
 import org.springframework.stereotype.Component;
 import ru.alvisid.pacs.model.enumActivate.AbstractDictionary;
 import ru.alvisid.pacs.model.enumActivate.MappedEnum;
@@ -12,7 +11,6 @@ import javax.persistence.metamodel.EntityType;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
@@ -33,54 +31,50 @@ public class EnumLoader {
                 .collect(Collectors.toList());
 
         for (Object obj : entityClasses) {
-            if (!AbstractDictionary.class.isAssignableFrom((Class<AbstractDictionary>)obj)) {
+            if (!AbstractDictionary.class.isAssignableFrom((Class <AbstractDictionary>) obj)) {
                 continue;
             }
 
-            if (!((Class)obj).isAnnotationPresent(MappedEnum.class)){
+            if (!((Class) obj).isAnnotationPresent(MappedEnum.class)) {
                 continue;
             }
 
-            MappedEnum mappedEnum = ((Class<MappedEnum>)obj).getAnnotation(MappedEnum.class);
-            Session session = ((Session)em.getDelegate()).getSessionFactory().openSession();
-            updateEnumIdentifiers(session, mappedEnum.enumClass(), (Class<AbstractDictionary>)obj);
+            MappedEnum mappedEnum = ((Class <MappedEnum>) obj).getAnnotation(MappedEnum.class);
+            updateEnumIdentifiers(mappedEnum.enumClass(), (Class <AbstractDictionary>) obj);
+            updateEnumValues(mappedEnum.enumClass());
         }
     }
 
-    private void updateEnumIdentifiers(Session session, Class <? extends Enum> enumClass,
+    private void updateEnumIdentifiers(Class <? extends Enum> enumClass,
                                        Class <? extends AbstractDictionary> dictClass) {
-        /*String sql = "FROM " + dictClass.getSimpleName();
-        List <AbstractDictionary> valueList = session.createQuery(sql).list();*/
         String sql = "FROM " + dictClass.getSimpleName();
         List <AbstractDictionary> valueList = em.createQuery(sql).getResultList();
-        Enum[] constants = enumClass.getEnumConstants();
-        int maxId = 0;
-        int valueId = 0;
 
         for (AbstractDictionary dict : valueList) {
-            valueId = dict.getId();
-            setEnumOrdinal(Enum.valueOf(enumClass, dict.getCode()), valueId);
-
-            if (valueId > maxId) {
-                maxId = valueId;
-            }
+            setEnumOrdinal(Enum.valueOf(enumClass, dict.getCode()), dict.getId());
         }
+    }
 
-        Object valuesArray = Array.newInstance(enumClass, maxId + 1);
+    private void updateEnumValues(Class <? extends Enum> enumClass) {
+        Enum[] enumConstants = enumClass.getEnumConstants();
 
-        for (Enum value : constants) {
+        Object valuesArray = Array.newInstance(enumClass, enumConstants.length + 1);
+
+        for (Enum value : enumConstants) {
             Array.set(valuesArray, value.ordinal(), value);
         }
 
         Field field;
+        Field modifiersField;
 
         try {
             field = enumClass.getDeclaredField("$VALUES");
             field.setAccessible(true);
 
-            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField = Field.class.getDeclaredField("modifiers");
             modifiersField.setAccessible(true);
             modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
             field.set(null, valuesArray);
         } catch (Exception exc) {
             throw new RuntimeException("Can't update values array: ", exc);
