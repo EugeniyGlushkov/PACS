26d079ae596142c6b580404bdcf7d1a75a901be0
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2018-12-04 08:16:41 +0300

    add DayOffRepository

---
+++ src/main/java/ru/alvisid/pacs/repository/DayOffRepository.java
@@ -0,0 +1,12 @@
+package ru.alvisid.pacs.repository;
+
+import ru.alvisid.pacs.model.DayOff;
+
+/**
+ * The generalized functional for day's off repository.
+ *
+ * @author Glushkov Evgeniy
+ * @version 1.0
+ */
+public interface DayOffRepository extends TypicalRepository<DayOff> {
+}
