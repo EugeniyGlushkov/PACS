799d7eac1a5588f2f3c05e09b18b99c186601f02
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2018-12-29 17:28:50 +0300

    Start DeptScheduleTestData.

--- src/test/java/testdata/DeptScheduleTestData.java
+++ src/test/java/testdata/DeptScheduleTestData.java
@@ -9,23 +9,33 @@ import static testdata.DepartmentTestData.*;
 public class DeptScheduleTestData extends AbstractTestData <DeptSchedule> {
 
     public static final DeptSchedule
-    DEPT_SCHEDULE_1 = new DeptSchedule(10001,
+            DEPT_SCHEDULE_1 = new DeptSchedule(10001,
             LocalTime.of(9, 0), LocalTime.of(18, 0),
             LocalTime.of(12, 0), LocalTime.of(13, 0),
             DEPARTMENT_1),
-    DEPT_SCHEDULE_2 = new DeptSchedule(10002,
-            LocalTime.of(8, 0), LocalTime.of(17, 0),
-            LocalTime.of(11, 0), LocalTime.of(12, 0),
-            DEPARTMENT_2);
+            DEPT_SCHEDULE_2 = new DeptSchedule(10002,
+                    LocalTime.of(8, 0), LocalTime.of(17, 0),
+                    LocalTime.of(11, 0), LocalTime.of(12, 0),
+                    DEPARTMENT_2);
+
+    public static final DeptSchedule NEW_DEPT_SCHEDULE_3 = new DeptSchedule(null,
+            LocalTime.of(9, 0), LocalTime.of(18, 0),
+            LocalTime.of(12, 30), LocalTime.of(13, 30),
+            DEPARTMENT_3);
+
+    public static final DeptSchedule UPDATED_DEPT_SCHEDULE_1 = new DeptSchedule(10001,
+            LocalTime.of(8, 20), LocalTime.of(17, 20),
+            LocalTime.of(13, 0), LocalTime.of(14, 0),
+            DEPARTMENT_1);
 
     @Override
     public DeptSchedule getNew() {
-        return null;
+        return new DeptSchedule(NEW_DEPT_SCHEDULE_3);
     }
 
     @Override
     public DeptSchedule getUpdated() {
-        return null;
+        return new DeptSchedule(UPDATED_DEPT_SCHEDULE_1);
     }
 
     @Override
