8140151b4716ff71fb7fddddf0f206102f30a2bf
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2019-02-06 21:55:55 +0300

    Continue PointPermitServiceTest.

--- src/test/java/ru/alvisid/pacs/service/PointPermitServiceTest.java
+++ src/test/java/ru/alvisid/pacs/service/PointPermitServiceTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import ru.alvisid.pacs.model.Employee;
 import ru.alvisid.pacs.model.PointAction;
 import ru.alvisid.pacs.model.PointPermit;
+import ru.alvisid.pacs.util.exceptions.NotFoundException;
 import testdata.PointPermitTestData;
 
 import static util.TestUtil.assertMatch;
@@ -37,13 +38,48 @@ public class PointPermitServiceTest extends AbstractServiceTest <PointPermit, Po
     @Test
     public void createWithEmpIdAndPointActId() {
         PointPermit expectedPointPermit = testData.getNew();
-        Employee expectedEmployee = expectedPointPermit.getEmployee();
-        PointAction expectedPointAct = expectedPointPermit.getPointAction();
+        int expectedEmployeeId = expectedPointPermit.getEmployee().getId();
+        int expectedPointActId = expectedPointPermit.getPointAction().getId();
         expectedPointPermit.setEmployee(null);
         expectedPointPermit.setPointAction(null);
-        PointPermit actualPointPermit = service.create(expectedPointPermit, expectedEmployee.getId(), expectedPointAct.getId());
+        PointPermit actualPointPermit = service.create(expectedPointPermit, expectedEmployeeId, expectedPointActId);
         expectedPointPermit = testData.getNew();
         expectedPointPermit.setId(actualPointPermit.getId());
         assertMatch(testData.IGNORING_FIELDS, service.getAll(), testData.getCreatedArray(expectedPointPermit));
+    }
+
+    /**
+     * Checks a matching the actual updated value from DB to the expected updated value from {@code testData}:
+     * update an exiting object with inserted {@code employee} {@code point action} in the DB;
+     * checks matching the actual list of all objects to the expected list of all objects from {@code testData}.
+     */
+    @Test
+    public void updateWithEmpIdAndPointActId() {
+        PointPermit expectedPointPermit = testData.getUpdated();
+        int expectedEmployeeId = expectedPointPermit.getEmployee().getId();
+        int expectedPointActId = expectedPointPermit.getPointAction().getId();
+        expectedPointPermit.setEmployee(null);
+        expectedPointPermit.setPointAction(null);
+        service.update(expectedPointPermit, expectedEmployeeId, expectedPointActId);
+        PointPermit actualPointPermit = service.get(expectedPointPermit.getId());
+        expectedPointPermit = testData.getUpdated();
+        assertMatch(testData.IGNORING_FIELDS, actualPointPermit, expectedPointPermit);
+    }
+
+    /**
+     * Checks the {@code NotFoundException} when there are no updated object's id (updated object
+     * with inserted employee and point action) in the DB.
+     */
+    @Test
+    public void updateWithEmpIdAndPointActIdNotFound() {
+        PointPermit expectedPointPermit = testData.getUpdated();
+        expectedPointPermit.setId(testData.NOT_FOUND_ID);
+        int expectedEmployeeId = expectedPointPermit.getEmployee().getId();
+        int expectedPointActId = expectedPointPermit.getPointAction().getId();
+        expectedPointPermit.setEmployee(null);
+        expectedPointPermit.setPointAction(null);
+        thrown.expect(NotFoundException.class);
+        thrown.expectMessage("Not found entity with id=" + testData.NOT_FOUND_ID);
+        service.update(expectedPointPermit, expectedEmployeeId, expectedPointActId);
     }
 }
